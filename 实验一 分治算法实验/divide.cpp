d2d3fa4180bfee7e80ef73667ee39447ed2b82e5
SDH <3147711404@qq.com>
2025-04-02 08:43:42 +0800

    /debug for循环i 的范围

--- 实验一 分治算法实验/divide.cpp
+++ 实验一 分治算法实验/divide.cpp
@@ -1,8 +1,5 @@
 #include"divide.h"
 #pragma once
-#include<iostream>
-#include<vector>
-#include<fstream>
 
 using namespace std;
 
@@ -10,7 +7,7 @@ bool readfile(vector<int>& T)
 {
 	ifstream fin("1.txt");
 	if (!fin) {
-		std::cerr << "无法打开文件" << std::endl;
+		cerr << "无法打开文件" << endl;
 		return 1;
 	}
 	int number;
@@ -30,18 +27,53 @@ void merge_sort(vector<int>& T, int head, int tail)
 	{
 		int mid = (head + tail) / 2;
 		merge_sort(T, head, mid);
-		merge_sort(T, mid+1, tail);
-		merge(T, head,mid, tail);
+		merge_sort(T, mid + 1, tail);
+		merge(T, head, mid, tail);
 	}
 }
 
-void merge(vector<int>& T, int head,int mid ,int tail)
+void merge(vector<int>& T, int head, int mid, int tail)
 {
-	vector<int>A ,B;
-	for (int i = head; i < mid; i++)
+	vector<int>A, B;
+	for (int i = head; i <= mid; i++)
 	{
-
+		A.push_back(T[i]);
+	}
+	for (int i = mid + 1; i <= tail; i++)
+	{
+		B.push_back(T[i]);
 	}
 
+	int a = 0;
+	int b = 0;
 
+	for (int i = 0; i <= tail; )
+	{
+		if (A[a] <= B[b])
+		{
+			T[i++] = A[a++];
+		}
+		else
+		{
+			T[i++] = B[b++];
+		}
+
+		if (a == A.size())
+		{
+			while (b < B.size())
+			{
+				T[i]=B[b++];
+			}
+			break;
+		}
+
+		if (b == B.size())
+		{
+			while (a < A.size())
+			{
+				T[i] = (A[a++]);
+			}
+			break;
+		}
+	}
 }
--- 实验一 分治算法实验/源.cpp
+++ 实验一 分治算法实验/源.cpp
@@ -7,11 +7,22 @@ int main()
 	 //读取文件
 	if (readfile(T))	//读取文件失败报错1
 	{
+
 		return 1;
 	}
-
+	
+	
+	// 随便加了一点数字，正常要报错但是
+	// 
+	// 
 	//递归形式的归并排序
 	merge_sort(T, 0, T.size() - 1);
+	
+	////if (T[0] != 1)
+	//{
+	//	cout << "ERRROR" << endl;
+	//	return 2;
+	//}
 
 	for (auto ch:T)
 	{
